f2986da219bb4daead00d6e1719806a9577c90b8
Lucy <ll7@princeton.edu>
2016-06-21 12:55:24 +0200

    Fixed glitches, Quizlet, copies to clipboard

--- contentscripts/paideiafy.js
+++ contentscripts/paideiafy.js
@@ -9,6 +9,30 @@
 console.log('running paideiafy.js');
 var event = 0;
 var lang;
+var saved = "";
+
+function quizlet() {
+  insertDiv('<div id="paideia-panel"><b>Just copy and paste the text below into the import space when creating a Quizlet set.</b><br>' + 
+    '<div id="vocab"><pre style="font-size: 12px; font-family: Geneva, sans-serif; text-align: left;">' + saved + '</pre></div>' + 
+    '<button id="copy">Copy vocabulary to clipboard!</button>' + 
+    '<button id="go-quizlet">Open Quizlet in new tab</button>' + 
+    '<button id="remove">Close</button></div>');
+  $('#remove').click(rmPanel);
+  document.getElementById('copy').addEventListener('click', function() {executeCopy(saved);});
+  document.getElementById('go-quizlet').addEventListener('click', function() {
+    window.open("https://quizlet.com/create-set", "Quizlet-Tab");
+  });
+}
+
+function executeCopy(text) {
+  var input = document.createElement('textarea');
+  document.getElementById('paideia-panel').appendChild(input);
+  input.value = text;
+  input.focus();
+  input.select();
+  document.execCommand('Copy');
+  input.remove();
+}
 
 function anotherDictionary (word) {
   return '<p>Try this word in another dictionary: </p>' +
@@ -74,6 +98,9 @@ function parseAjax(word, toReturn) {
   resultFound = perseus.find('.lemma').html(); // will be undefined if perseus finds no results
   if (resultFound) {
     var header = lemma.find('.lemma_header').prop('outerHTML');
+    var def = lemma.find('.lemma_definition')[0];
+    saved += word + '\t' + def.innerHTML.trim() + '\n';
+    console.log(saved);
     table = lemma.find('table').addClass('paideia-table').prop('outerHTML');
     insertDiv('<div id="paideia-panel"><button id="remove" style="float: right;">X</button>' + header + "<br />" + table + anotherDictionary(word) + thanks + '</div>');
     $('#remove').click(rmPanel);
